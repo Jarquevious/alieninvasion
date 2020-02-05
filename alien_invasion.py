73eea8272402a8f3bcd568a4a62f4cd939da0827
Jarquevious <jarquevious@gmail.com>
2020-02-04 23:59:51 -0800

    first file complete

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,4 +4,13 @@ import pygame
 def run_game():
     # Initialize game and create a screen object. 
     pygame.init()
-    screen
+    screen = pygame.display.set_mode((1200,800))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Start the main loop for the game.
+    While True:
+
+        # Watch for keyboard and mouse events. 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
