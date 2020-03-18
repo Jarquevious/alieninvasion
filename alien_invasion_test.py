c003c898b5886e23b401ae1211672e0cdeec0cd5
Jarquevious <jarquevious@gmail.com>
2020-03-18 15:23:06 -0700

    continuous movement to the right

---
+++ alien_invasion_test.py
@@ -0,0 +1,5 @@
+import unittest
+import alien_invasion   
+
+class Test_run_game(unittest.TestCase):
+    def test_run_game(self):
--- game_functions.py
+++ game_functions.py
@@ -9,6 +9,11 @@ def check_events(ship):
 
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
+                    ship.moving_right = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    ship.moving_right = False
                     # Move the ship to the right.
                     ship.rect.centerx += 1
 
