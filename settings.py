4619fa05c623205b464c1f37172b8816ef0fe17b
Jarquevious <jarquevious@gmail.com>
2020-03-18 17:05:52 -0700

    speed ajdustment added

--- settings.py
+++ settings.py
@@ -9,6 +9,6 @@ class Settings():
         self.bg_color = (200,230,230)
 
         # Ship settings
-        self.ship_speed_factor = 1.5
+        self.ship_speed_factor = 4.5
 
         
--- ship.py
+++ ship.py
@@ -2,9 +2,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Initialized the ship and set its startying position."""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
@@ -15,6 +16,9 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # Store a decimal value for the ship's center
+        self.center = float(self.rect.centerx)
+
         # Movement Flag
         self.moving_right = False
         self.moving_left = False
@@ -23,10 +27,14 @@ class Ship():
 
     def update(self):
         """Updates the ship's position based on the movement flag."""
+        # Update the ships center value, not the rect.
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor 
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # Update rect object from self.center.
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Draw the ship at sits current location."""
